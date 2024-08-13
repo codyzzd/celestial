33f6deea87be7ebe9186cf08547cb98f337d0805
codyzzd <codyzzd@gmail.com>
2024-08-13 15:57:19 -0300

    switch drawer to modal passengers

--- app/passengers.php
+++ app/passengers.php
@@ -94,158 +94,173 @@ $sexs = getSexs();
         </div>
       </div>
 
-      <!-- drawer component -->
-      <div id="passengers_add_modal"
-           class="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-80 dark:bg-gray-800"
+      <!-- modal add -->
+      <div id="passenger_add_modal"
+           data-modal-placement="bottom-center"
            tabindex="-1"
-           aria-labelledby="drawer-right-label">
-        <h5 id="drawer-label"
-            class="inline-flex items-center mb-6 text-sm font-semibold text-gray-500 uppercase dark:text-gray-400">Adicionar Pessoa</h5>
-        <button type="button"
-                data-drawer-dismiss="passengers_add_modal"
-                aria-controls="passengers_add_modal"
-                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
-          <svg aria-hidden="true"
-               class="w-5 h-5"
-               fill="currentColor"
-               viewBox="0 0 20 20"
-               xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"></path>
-          </svg>
-          <span class="sr-only">Close menu</span>
-        </button>
-        <form id="passenger_add">
-          <div class="space-y-4">
-
-            <div class="col-span-2">
-              <label for="name"
-                     class="block mb-2 text-sm font-medium text-gray-900">Nome Completo</label>
-              <input type="text"
-                     id="name"
-                     name="name"
-                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                     placeholder="ex: Bruno da Silva Gonçalves"
-                     required
-                     autocomplete="off">
+           aria-hidden="true"
+           class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-md max-h-full">
+          <!-- Modal content -->
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5  rounded-t dark:border-gray-600 border-b">
+              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                Adicionar Pessoa
+              </h3>
+              <button type="button"
+                      class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                      data-modal-toggle="passenger_add_modal">
+                <svg class="w-3 h-3"
+                     aria-hidden="true"
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill="none"
+                     viewBox="0 0 14 14">
+                  <path stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span class="sr-only">Fechar Modal</span>
+              </button>
             </div>
+            <!-- Modal body -->
+            <form class=""
+                  id="passenger_add">
+              <div class="grid gap-4 mb-4 grid-cols-2 p-4">
 
-            <div class="col-span-2">
-              <label for="nasc_date"
-                     class="block mb-2 text-sm font-medium text-gray-900">Data de Nascimento</label>
-              <input type="text"
-                     id="nasc_date"
-                     name="nasc_date"
-                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                     placeholder="dd/mm/aaaa"
-                     required
-                     autocomplete="off">
-            </div>
+                <div class="col-span-2">
+                  <label for="name"
+                         class="block mb-2 text-sm font-medium text-gray-900">Nome Completo</label>
+                  <input type="text"
+                         id="name"
+                         name="name"
+                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                         placeholder="ex: Bruno da Silva Gonçalves"
+                         required
+                         autocomplete="off">
+                </div>
 
-            <div class="col-span-2">
-              <label for="inline-radio-group"
-                     class="block mb-2 text-sm font-medium text-gray-900">Gênero</label>
-              <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex">
-                <?php
-                // Iterar sobre os sexs
-                foreach ($sexs as $sex):
-                  // Usar sintaxe ternária para definir ícone e cor
-                  $icon = ($sex['slug'] == 'masc') ? 'fa-mars' : (($sex['slug'] == 'femi') ? 'fa-venus' : '');
-                  $color = ($sex['slug'] == 'masc') ? 'text-blue-700' : (($sex['slug'] == 'femi') ? 'text-pink-700' : '');
-                  ?>
-                  <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
-                    <div class="flex items-center ps-3">
-                      <input id="horizontal-list-radio-<?php echo $sex['slug']; ?>"
-                             type="radio"
-                             value="<?php echo $sex['id']; ?>"
-                             name="sex"
-                             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
-                             required>
-                      <label for="horizontal-list-radio-<?php echo $sex['slug']; ?>"
-                             class="w-full py-3 ms-2 text-sm font-medium text-gray-900">
-                        <i class="fa <?php echo $icon; ?> fa-fw <?php echo $color; ?>"></i>
-                        <?php echo $sex['name']; ?>
-                      </label>
-                    </div>
-                  </li>
-                <?php endforeach; ?>
+                <div class="col-span-2">
+                  <label for="nasc_date"
+                         class="block mb-2 text-sm font-medium text-gray-900">Data de Nascimento</label>
+                  <input type="text"
+                         id="nasc_date"
+                         name="nasc_date"
+                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                         placeholder="dd/mm/aaaa"
+                         required
+                         autocomplete="off">
+                </div>
 
-              </ul>
-            </div>
+                <div class="col-span-2">
+                  <label for="inline-radio-group"
+                         class="block mb-2 text-sm font-medium text-gray-900">Gênero</label>
+                  <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex">
+                    <?php
+                    // Iterar sobre os sexs
+                    foreach ($sexs as $sex):
+                      // Usar sintaxe ternária para definir ícone e cor
+                      $icon = ($sex['slug'] == 'masc') ? 'fa-mars' : (($sex['slug'] == 'femi') ? 'fa-venus' : '');
+                      $color = ($sex['slug'] == 'masc') ? 'text-blue-700' : (($sex['slug'] == 'femi') ? 'text-pink-700' : '');
+                      ?>
+                      <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
+                        <div class="flex items-center ps-3">
+                          <input id="horizontal-list-radio-<?php echo $sex['slug']; ?>"
+                                 type="radio"
+                                 value="<?php echo $sex['id']; ?>"
+                                 name="sex"
+                                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
+                                 required>
+                          <label for="horizontal-list-radio-<?php echo $sex['slug']; ?>"
+                                 class="w-full py-3 ms-2 text-sm font-medium text-gray-900">
+                            <i class="fa <?php echo $icon; ?> fa-fw <?php echo $color; ?>"></i>
+                            <?php echo $sex['name']; ?>
+                          </label>
+                        </div>
+                      </li>
+                    <?php endforeach; ?>
 
-            <div class="col-span-2">
-              <label for="id_ward"
-                     class="block mb-2 text-sm font-medium text-gray-900">Qual ala pertence?</label>
-              <select id="id_ward"
-                      name="id_ward"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                      required>
-                <option value=""
-                        selected>Selecione...</option>
-                <?php foreach ($wards as $ward): ?>
-                  <option value="<?php echo $ward['id']; ?>"><?php echo $ward['name']; ?></option>
-                <?php endforeach; ?>
-              </select>
-            </div>
+                  </ul>
+                </div>
 
-            <div class="col-span-2">
-              <label for="id_document"
-                     class="block mb-2 text-sm font-medium text-gray-900">Tipo de Documento</label>
-              <select id="id_document"
-                      name="id_document"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                      required>
-                <option value=""
-                        selected>Selecione...</option>
-                <?php foreach ($documents as $document): ?>
-                  <option value="<?php echo $document['id']; ?>"><?php echo $document['name']; ?></option>
-                <?php endforeach; ?>
-              </select>
-            </div>
+                <div class="col-span-2">
+                  <label for="id_ward"
+                         class="block mb-2 text-sm font-medium text-gray-900">Qual ala pertence?</label>
+                  <select id="id_ward"
+                          name="id_ward"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                          required>
+                    <option value=""
+                            selected>Selecione...</option>
+                    <?php foreach ($wards as $ward): ?>
+                      <option value="<?php echo $ward['id']; ?>"><?php echo $ward['name']; ?></option>
+                    <?php endforeach; ?>
+                  </select>
+                </div>
 
-            <div class="col-span-2">
-              <label for="document"
-                     class="block mb-2 text-sm font-medium text-gray-900">Documento</label>
-              <input type="text"
-                     id="document"
-                     name="document"
-                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                     required
-                     autocomplete="off">
-            </div>
+                <div class="col-span-2">
+                  <label for="id_document"
+                         class="block mb-2 text-sm font-medium text-gray-900">Tipo de Documento</label>
+                  <select id="id_document"
+                          name="id_document"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                          required>
+                    <option value=""
+                            selected>Selecione...</option>
+                    <?php foreach ($documents as $document): ?>
+                      <option value="<?php echo $document['id']; ?>"><?php echo $document['name']; ?></option>
+                    <?php endforeach; ?>
+                  </select>
+                </div>
 
-            <div class="col-span-2">
-              <label for="fever_date"
-                     class="block mb-2 text-sm font-medium text-gray-900">Data da Vacina da Febre Amarela</label>
-              <input type="text"
-                     id="fever_date"
-                     name="fever_date"
-                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                     placeholder="dd/mm/aaaa"
-                     autocomplete="off">
-            </div>
+                <div class="col-span-2">
+                  <label for="document"
+                         class="block mb-2 text-sm font-medium text-gray-900">Documento</label>
+                  <input type="text"
+                         id="document"
+                         name="document"
+                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                         required
+                         autocomplete="off">
+                </div>
 
-            <div class="col-span-2">
-              <label for="obs"
-                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Observação</label>
-              <textarea id="obs"
-                        name="obs"
-                        rows="4"
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
-            </div>
+                <div class="col-span-2">
+                  <label for="fever_date"
+                         class="block mb-2 text-sm font-medium text-gray-900">Data da Vacina da Febre Amarela</label>
+                  <input type="text"
+                         id="fever_date"
+                         name="fever_date"
+                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                         placeholder="dd/mm/aaaa"
+                         autocomplete="off">
+                </div>
 
+                <div class="col-span-2">
+                  <label for="obs"
+                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Observação</label>
+                  <textarea id="obs"
+                            name="obs"
+                            rows="4"
+                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+                </div>
+
+              </div>
+
+              <!-- Modal footer -->
+              <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600 justify-end gap-3">
+                <button type="button"
+                        data-modal-toggle="passenger_add_modal"
+                        class="px-5 py-2.5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancelar</button>
+                <button type="submit"
+                        class=" px-5 py-2.5 text-sm font-medium inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-white text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Adicionar Ala
+                </button>
+              </div>
+            </form>
           </div>
-          <div class="bottom-0 left-0 flex justify-end w-full pb-4 mt-4 space-x-4 sm:absolute">
-            <button type="button"
-                    data-drawer-toggle="passengers_add_modal"
-                    class="px-5 py-2.5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancelar</button>
-            <button type="submit"
-                    class=" px-5 py-2.5 text-sm font-medium inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-white text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              Adicionar Pessoa
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
 
     </section>
