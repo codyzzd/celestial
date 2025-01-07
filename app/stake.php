b0449dd932377ae23bfd60ccb279506ca6d9e1eb
codyzzd <codyzzd@gmail.com>
2025-01-06 21:08:48 -0300

    stake permission approve access

--- app/stake.php
+++ app/stake.php
@@ -69,6 +69,12 @@ $user_role = checkUserRole($user_id, ['stake_lider', 'stake_aux']);
               Caravanas</span>
             <i class="fa fa-chevron-right text-lg text-gray-500"></i>
           </a>
+          <a href="ward_caravans_approve.php"
+             class="block w-full px-4 py-2  border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-700 focus:text-purple-700 flex justify-between">
+            <span><i class="fa fa-check-to-slot text-lg text-gray-500 fa-fw me-2"></i>
+              Aprovar Reservas</span>
+            <i class="fa fa-chevron-right text-lg text-gray-500"></i>
+          </a>
           <?php if (in_array($user_role, ['stake_lider'])): ?>
             <a href="stake_users.php"
                class="block w-full px-4 py-2  border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-700 focus:text-purple-700 flex justify-between">
