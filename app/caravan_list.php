ab138e006688145f7aaa90a634b507545fdd1a22
codyzzd <codyzzd@gmail.com>
2025-02-10 20:54:40 -0300

    sortable icon 2

--- app/caravan_list.php
+++ app/caravan_list.php
@@ -207,7 +207,7 @@ foreach ($seats as $seat) {
                 <th scope="col"
                     class="p-3 text-nowrap">#</th>
                 <th scope="col"
-                    class="p-3 text-nowrap ">Nome <a href="#"><svg class="w-3 h-3 ms-1.5"
+                    class="p-3 text-nowrap flex items-center">Nome <a href="#"><svg class="w-3 h-3 ms-1.5"
                          aria-hidden="true"
                          xmlns="http://www.w3.org/2000/svg"
                          fill="currentColor"
@@ -215,7 +215,7 @@ foreach ($seats as $seat) {
                       <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
                     </svg></a></th>
                 <th scope="col"
-                    class="p-3 text-nowrap">Idade <a href="#"><svg class="w-3 h-3 ms-1.5"
+                    class="p-3 text-nowrap flex items-center">Idade <a href="#"><svg class="w-3 h-3 ms-1.5"
                          aria-hidden="true"
                          xmlns="http://www.w3.org/2000/svg"
                          fill="currentColor"
@@ -223,7 +223,7 @@ foreach ($seats as $seat) {
                       <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
                     </svg></a></th>
                 <th scope="col"
-                    class="p-3 text-nowrap ">Ala<a href="#"><svg class="w-3 h-3 ms-1.5"
+                    class="p-3 text-nowrap flex items-center">Ala<a href="#"><svg class="w-3 h-3 ms-1.5"
                          aria-hidden="true"
                          xmlns="http://www.w3.org/2000/svg"
                          fill="currentColor"
